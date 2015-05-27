469edab6f741462eff064725b60ebc90f3b8fb92
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-05-27 17:16:54 -0300

    fix logrotate wildcard

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures mo_reverse_proxy'
 long_description 'Installs/Configures mo_reverse_proxy'
 
-version          '1.1.5'
+version          '1.1.6'
 
 depends   'certificate',      '~>0.6.3'
 depends   'mo_application',   '~>1.1.1'
