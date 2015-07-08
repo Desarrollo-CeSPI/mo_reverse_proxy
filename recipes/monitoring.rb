1469e01d85b25e94ee2b6a5d60b142e8901516d3
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-07-08 12:38:31 -0300

    Agregado de monitoreo de status code y locations
    Se agrega un chequeo del status code de cada vhost y además permite que
    el databag especifique locations customizables. Ver ejemplo de vagrant

--- recipes/monitoring.rb
+++ recipes/monitoring.rb
@@ -1,20 +1,14 @@
+require 'chef/mixin/shell_out'
+
+shell_out("rm #{node['nrpe']['conf_dir']}/nrpe.d/{ssl-cert,check-http}_*").run_command
+
 include_recipe "mo_monitoring_client"
+
 node['mo_reverse_proxy']['applications'].each do |app|
   mo_reverse_proxy_block(app) do |id, data|
 
-    if data['ssl']
+    mo_reverse_proxy_monitor_http(data)
+    mo_reverse_proxy_monitor_ssl(data) if data['ssl']
 
-      server_name = Array(data['server_name']).first
-      check_name = "ssl-cert_#{server_name}"
-
-      nrpe_check check_name do
-        command "#{node["mo_monitoring_client"]["install_directory"]}/check_ssl_cert"
-        warning_condition node['mo_monitoring_client']['plugins']['check_ssl_cert']['warning_condition']
-        critical_condition node['mo_monitoring_client']['plugins']['check_ssl_cert']['critical_condition']
-        parameters "-A -H #{server_name}"
-        notifies :restart, "service[#{node['nrpe']['service_name']}]"
-      end
-
-    end
   end
 end
