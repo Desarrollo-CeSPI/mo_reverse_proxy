f2323140671602cfc52349545a15a41a61d6a380
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-12-22 14:46:13 -0300

    menos dias de rotacion

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -5,7 +5,7 @@ logrotate_app "mo-reverse-proxy" do
   options   %w(missingok delaycompress notifempty compress sharedscripts)
   frequency 'weekly'
   minsize   '1M'
-  rotate    52
+  rotate    10
   create    %W(644 #{www_user} #{www_group}).join ' '
   postrotate "    [ -s #{nginx_pid} ] && kill -USR1 `cat #{nginx_pid}`"
 end
